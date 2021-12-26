27027ed3ae3d55a8c558fa7b69c1e6a21ae9ec99
mac <awonusiolajide@yahoo.com>
2021-12-27 00:45:31 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=1cuc4klqgVNYFnU0GvwD
+export POSTGRES_HOST=database-2.cvs9gt75yffc.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=project_december
+export AWS_BUCKET=udagram-dec
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
