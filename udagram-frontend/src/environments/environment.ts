489e45b72481a8cc07235bbb326b26ac792b483c
mac <awonusiolajide@yahoo.com>
2021-12-28 00:40:14 +0100

    k8 deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8cd4e47e2eb64f1faf8f44d5c3960a7-1718425521.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
